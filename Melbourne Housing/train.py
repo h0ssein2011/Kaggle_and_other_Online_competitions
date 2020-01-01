f4c2f0ec140045d02e89fdf0b57cef767e619907
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-01 18:40:02 +0330

    Update py file

--- Melbourne Housing/train.py
+++ Melbourne Housing/train.py
@@ -16,8 +16,34 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test
 
 # fucntion to measure the models performance
 
-from sklearn.ensmeble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error as mea
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
-def score_datasets(X_train, X_valid,y_train, y_valid ):
-    model = RandomForestRegressor(n_estim)
+def score_dataset(X_train, X_valid,y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=10 ,random_state=0 )
+    model.fit(X_train , y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+#approaches with null vlas columns
+
+#1-delete cols with null vals
+
+
+cols_with_missing_vals = [col for col in X_train.columns if X_train[col].isnull().any()]
+
+reduced_X_train=X_train.drop(cols_with_missing_vals , axis=1)
+reduced_X_valid=X_valid.drop(cols_with_missing_vals , axis=1)
+
+reduced_error = score_dataset(reduced_X_train ,reduced_X_valid,y_train,y_valid)
+#
+# print('Meae in dropping cols with null values :{}'.format(reduced_error))
+
+
+# Drop columns in training and validation data
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+#2-Imputation
+
+
