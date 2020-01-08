b51e5208d677091712412b56b2705da3069417e8
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-08 17:38:24 +0330

    update sourse data

--- Ames Housing dataset/Handeling_categorical_variables.py
+++ Ames Housing dataset/Handeling_categorical_variables.py
@@ -1,20 +1,21 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
-df=pd.read_csv('Input/train.csv' , index_col='Id')
-df_test = pd.read_csv('Input/test.csv' , index_col='Id')
-
-X=df.drop('SalePrice',axis=1)
-y=df['SalePrice']
-
-
+df=pd.read_csv('Input/melb_data.csv')
+print(df.columns)
 #
-
-s=(X.select_dtypes == 'object')
-object_cols = list(s[s].index)
-
-print('object_cols are:{}'.format(object_cols))
+X=df.drop('Price',axis=1)
+y=df['Price']
+#
+# #
+X_train,X_valid,y_train,y_valid =train_test_split(X,y,random_state=0,train_size=0.8)
+#
+# s=(X_train.dtypes == 'object')
+# object_cols = list(s[s].index)
+#
+# print('object_cols are:{}'.format(object_cols))
 
 
