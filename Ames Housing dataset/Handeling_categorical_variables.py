32205e7c7fd5d3d01773f34df3580359c830ef6d
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-11 17:54:49 +0330

    add drop and Label encoding approach

--- Ames Housing dataset/Handeling_categorical_variables.py
+++ Ames Housing dataset/Handeling_categorical_variables.py
@@ -32,7 +32,39 @@ X_valid = X_valid[my_cols]
 
 # list of categorical vars
 s =(X_train.dtypes == 'object')
-t=list(s[s].index)
-# object_cols =list(s[s].index)
-print(t)
-# print('object cols are:'.format(object_cols))
+object_cols=list(s[s].index)
+
+
+#import Model and Metrics
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=10, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+#approach 1:delete categorical variables
+droped_X_train = X_train.select_dtypes(exclude = 'object')
+droped_X_valid = X_valid.select_dtypes(exclude = 'object')
+
+drop_MAE = score_dataset(droped_X_train ,droped_X_valid,y_train,y_valid)
+print('MAE dropped approach: {}'.format(drop_MAE))
+
+
+#approach2:label encoding
+label_X_train = X_train.copy()
+label_X_valid =X_valid.copy()
+from sklearn.preprocessing import LabelEncoder
+
+Encoder =LabelEncoder()
+
+for col in object_cols:
+    label_X_train[col]=Encoder.fit_transform(label_X_train[col])
+    label_X_valid[col]=Encoder.fit_transform(label_X_valid[col])
+
+label_approach_MAE= score_dataset(label_X_train,label_X_valid,y_train,y_valid)
+print('MAE lable endcoding approach: {}'.format(label_approach_MAE))
+
+
