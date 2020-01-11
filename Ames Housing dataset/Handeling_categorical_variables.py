d7d99ffbe8d0cd9c9fbbd23dafdb94b7fd71ed44
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-11 12:25:45 +0330

    calculate Confusion Matrix in a sample problem

--- Ames Housing dataset/Handeling_categorical_variables.py
+++ Ames Housing dataset/Handeling_categorical_variables.py
@@ -13,6 +13,27 @@ y=df['Price']
 # #
 X_train,X_valid,y_train,y_valid =train_test_split(X,y,random_state=0,train_size=0.8)
 #
+# return cols with null vals
+null_cols = [col for col in X_train.columns if X_train[col].isnull().any()]
+print(null_cols)
+
+
+X_train_reduced = X_train.drop(null_cols ,axis=1, inplace =True)
+X_valid_reduced = X_valid.drop(null_cols ,axis=1, inplace =True)
+
+#select low cardinality cols
+low_card_cols = [cname for cname in X_train.columns if X_train[cname].nunique() < 10 and X_train[cname].dtype == 'object']
+print('test')
+int_cols = [cname for cname in X_train.columns if  X_train[cname].dtype in ('int64','float64')]
+
+
+
+
+
+
+
+
+
 # s=(X_train.dtypes == 'object')
 # object_cols = list(s[s].index)
 #
