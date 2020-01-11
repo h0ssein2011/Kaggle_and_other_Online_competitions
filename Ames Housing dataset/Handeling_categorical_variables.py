e31d2891ace08fedd64ed0d64d67fbb8a5d34931
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-11 16:45:26 +0330

    find categorical values

--- Ames Housing dataset/Handeling_categorical_variables.py
+++ Ames Housing dataset/Handeling_categorical_variables.py
@@ -15,7 +15,7 @@ X_train,X_valid,y_train,y_valid =train_test_split(X,y,random_state=0,train_size=
 #
 # return cols with null vals
 null_cols = [col for col in X_train.columns if X_train[col].isnull().any()]
-print(null_cols)
+print('null cols are:',null_cols)
 
 
 X_train_reduced = X_train.drop(null_cols ,axis=1, inplace =True)
@@ -23,20 +23,16 @@ X_valid_reduced = X_valid.drop(null_cols ,axis=1, inplace =True)
 
 #select low cardinality cols
 low_card_cols = [cname for cname in X_train.columns if X_train[cname].nunique() < 10 and X_train[cname].dtype == 'object']
-print('test')
-int_cols = [cname for cname in X_train.columns if  X_train[cname].dtype in ('int64','float64')]
+int_cols = [cname for cname in X_train.columns if X_train[cname].dtype in ('int64','float64')]
+# keep selected columns only
+my_cols = low_card_cols + int_cols
 
+X_train = X_train[my_cols]
+X_valid = X_valid[my_cols]
 
-
-
-
-
-
-
-
-# s=(X_train.dtypes == 'object')
-# object_cols = list(s[s].index)
-#
-# print('object_cols are:{}'.format(object_cols))
-
-
+# list of categorical vars
+s =(X_train.dtypes == 'object')
+t=list(s[s].index)
+# object_cols =list(s[s].index)
+print(t)
+# print('object cols are:'.format(object_cols))
