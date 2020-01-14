e539f54ec681c1512d89e72c4d5ce0a35370aeb4
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-14 17:10:17 +0330

    filter cat and int coll

--- Ames Housing dataset/using_pipeline_in_a_test_file.py
+++ Ames Housing dataset/using_pipeline_in_a_test_file.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import numpy as np 
+from sklearn.model_selection import train_test_split
+# 
 
-df =pd.read_csv('Input/train.csv')
-print(df.head())
+
+
+train =pd.read_csv('Input/train.csv')
+test=pd.read_csv('Input/test.csv')
+# print(train.columns)
+X = train.drop('SalePrice',axis = 1)
+
+y=train['SalePrice']
+
+X_train ,X_valid,y_train,y_valid = train_test_split(X,y,random_state=0 ,train_size =0.7 )
+
+#select cols with null vals
+null_cols = [col for col in X_train.columns if X_train[col].isnull().any()>0]
+
+#select low cardiality cols 
+cat_cols = [cname for cname in X_train.columns if X_train[cname].dtype == 'object' and X_train[cname].nunique() < 10 ]
+int_cols = [col for col in X_train.columns if X_train[col].dtype in ('int64','float64')]
