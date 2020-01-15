815a8ba15f54a92a0cf18837552bf494e7acbf47
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-15 18:30:36 +0330

    use pipeline to bundle preprocessing and model part

--- Ames Housing dataset/using_pipeline_in_a_test_file.py
+++ Ames Housing dataset/using_pipeline_in_a_test_file.py
@@ -3,8 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 # 
 
-
-
 train =pd.read_csv('Input/train.csv')
 test=pd.read_csv('Input/test.csv')
 # print(train.columns)
@@ -17,6 +15,52 @@ X_train ,X_valid,y_train,y_valid = train_test_split(X,y,random_state=0 ,train_si
 #select cols with null vals
 null_cols = [col for col in X_train.columns if X_train[col].isnull().any()>0]
 
+X_train.drop(null_cols ,axis = 1 , inplace =True)
+X_valid.drop(null_cols ,axis = 1 , inplace =True)
+    
 #select low cardiality cols 
 cat_cols = [cname for cname in X_train.columns if X_train[cname].dtype == 'object' and X_train[cname].nunique() < 10 ]
 int_cols = [col for col in X_train.columns if X_train[col].dtype in ('int64','float64')]
+
+my_cols = cat_cols + int_cols
+
+#replace cat and int cols with current cols
+X_train=X_train[my_cols]
+X_valid = X_valid[my_cols]
+
+
+## preprocessing part
+# import required packages
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+#import pipeline packaegs
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+#int cols preprpcessing
+num_transfomer = SimpleImputer(strategy='constant')
+
+#catecorical values
+cat_preprocessings = Pipeline(steps=[('imputer',SimpleImputer(strategy='most_frequent')),
+                                     ('one_hot',OneHotEncoder(handle_unknown='ignore'))])
+
+
+#bundle all preprocessing in a single line!
+preprcessing_part = ColumnTransformer(transformers=[('num',num_transfomer,int_cols),('cat',cat_preprocessings,cat_cols)])
+
+#define the model
+from sklearn.ensemble import RandomForestRegressor
+
+model = RandomForestRegressor(n_estimators=100 , random_state=0)
+
+#bundel prepocessing and model in one line 
+clf =Pipeline(steps=[('preprocess',preprcessing_part),('model',model)])
+
+#predict by pipelined model
+pereds = clf.predict(X_valid)
+
+#evaluate the model
+from sklearn.metrics import mean_absolute_error
+
+print('MAE Pipllined model is: {}',format(mean_absolute_error(y_valid,pereds)))
