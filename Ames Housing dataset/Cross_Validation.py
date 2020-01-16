ccad62072529be5ddc40fce90212f5f3ba9d17c6
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-16 22:22:09 +0330

    use cross validation in Melb dataset

--- Ames Housing dataset/Cross_Validation.py
+++ Ames Housing dataset/Cross_Validation.py
@@ -13,7 +13,7 @@ X= df[cols_to_use]
 y=df['Price']
 
 #1- find null values
-null_cols = [col for col in X if X[col].isull().any()]
+null_cols = [col for col in X if X[col].isnull().any()]
 #1.1- fill nulls 
 
 # postpone to pipeline step
@@ -29,18 +29,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 
-my_pipeline = Pipeline(steps=[('preprocessing',SimpleImputer),
+my_pipeline = Pipeline(steps=[('preprocessing',SimpleImputer()),
                                 ('model',RandomForestRegressor(n_estimators=50 , random_state=0))])
 
 #3.2 Corss validation
 from sklearn.model_selection import cross_val_score
 
-scores = -1 * cross_val_score(my_pipeline , X,y, cv=5 ,scoring='')
+scores = -1 * cross_val_score(my_pipeline , X,y, cv=5 ,scoring='neg_mean_absolute_error')
 #4- Fit model / 
-
+#done in step 3
 
 # 5- ealuation 
 
-
+print('MAE is:\n',scores)
+#lets use avg scores
+print('Avg score is:\n',scores.mean())
 #6 submuission (if so)
 
+#not for this problem 
