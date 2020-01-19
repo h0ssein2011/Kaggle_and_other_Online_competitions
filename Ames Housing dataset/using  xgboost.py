5eeb3e9a2b8400e37412325c30e52f26966ad5df
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-19 18:06:04 +0330

    comapre Xgboost and RF in a sample dataset

--- Ames Housing dataset/using  xgboost.py
+++ Ames Housing dataset/using  xgboost.py
@@ -9,7 +9,8 @@ df = pd.read_csv('Input/melb_data.csv')
 selected_cols = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
 
 X=df[selected_cols]
-y=df.Price 
+y=df['Price']
+
 
 from sklearn.model_selection import  train_test_split
 X_train,X_valid ,y_train,y_valid =train_test_split(X,y,train_size=0.7 , random_state=0)
@@ -31,3 +32,30 @@ preds = my_model.predict(X_valid)
 from sklearn.metrics import mean_absolute_error
 MAE= mean_absolute_error(y_valid, preds)
 print('MAE xgboost is:{}'.format(MAE))
+
+#compare with RF 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+
+my_imputer = SimpleImputer()
+X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+X_valid = pd.DataFrame(my_imputer.fit_transform(X_valid))
+
+
+
+RF_model =RandomForestRegressor()
+RF_model.fit(X_train,y_train)
+RF_preds = RF_model.predict(X_valid)
+RF_MAE = mean_absolute_error(y_valid, RF_preds)
+print('MAE RF is: {}'.format(RF_MAE))
+
+#use Xgboost with Parameters
+
+modified_model= XGBRegressor(n_estimators=1000 , learning_rate= 0.05,n_jobs=4)
+modified_model.fit(X_train,y_train,early_stopping_rounds=5,
+                    eval_set=[(X_valid,y_valid)],
+                    verbose=False)
+
+modified_preds = modified_model.predict(X_valid)
+MAE_Xgb_modified =mean_absolute_error(y_valid,modified_preds)
+print('MAE Xgb_modified is:{}'.format(MAE_Xgb_modified))
