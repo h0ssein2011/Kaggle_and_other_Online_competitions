131f6d0155f3c38950d71555151f59d58c13c028
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-22 18:35:11 +0330

    leakage data in practice

--- Ames Housing dataset/Data Leakage.py
+++ Ames Housing dataset/Data Leakage.py
@@ -27,4 +27,23 @@ my_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
 socres = cross_val_score(my_pipeline,X,y,cv=5 ,scoring='accuracy')
 # 6- ealuation 
 print('cross validation accuracy:{}'.format(socres.mean()))
+
+# the accuracy is so high(98%) that is rare so lets have an investigation to data leakage
+
+card_data['has_expenditure'] = np.where(card_data['expenditure']>0,1,0)
+print(pd.crosstab(card_data.card,card_data.has_expenditure,margins=True))
+
+#it seems that 0% of those who had not card had expenditure vs 2% who had a card
+# so the expenditure and the corelated fatures related to this feature should be removed
+
+potential_leakage_cols =['expenditure','share', 'active', 'majorcards']
+
+X2 =X.drop(potential_leakage_cols,axis =1)
+score2=cross_val_score(RandomForestClassifier(n_estimators=100),X2,y,cv=5 , scoring='accuracy')
+
+print('new_score is: {}'.format(score2.mean()))
+
+# the accuracy decreased but it is better the leaked data
+
+
 #7 submuission (if so)
