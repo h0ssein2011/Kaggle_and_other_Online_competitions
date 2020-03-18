4b2d58b3c7b426b8c7ea01fc4edade1de4762f91
h0ssein2011 <h0ssein2011@yahoo.com>
2020-03-18 20:06:47 +0330

    Amazon dataset

---
+++ Amazon_com/Model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
+
+
+for col in train.columns:
+    print(col,'has :' , len(train[col].unique()))
+
